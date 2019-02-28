37e3dc394de247ce96f538e772c6b1bb62886888
yiny0liuyin <yi00oi9hhg@qq.com>
2019-02-28 18:35:28 +0800

    Ugh. Bad rebase.

--- header.php
+++ header.php
@@ -2,15 +2,15 @@
 <?php require_once 'functions.php'; ?>
 <!DOCTYPE HTML>
 <?php
-    require_once 'config.php';
-    if ($GLOBALS['style_BG'] != '') {
-        echo '<style>';
-        echo "\n";
-        echo 'body{background: #fff;}body::before {background: url('.$GLOBALS['style_BG'].') center/cover no-repeat;}blockquote::before {background: transparent !important;}';
-        echo "\n";
-        echo '</style>';
-        echo "\n";
-    }
+require_once 'config.php';
+if ($GLOBALS['style_BG'] != '') {
+    echo '<style>';
+    echo "\n";
+    echo 'body{background: #fff;}body::before {background: url('.$GLOBALS['style_BG'].') center/cover no-repeat;}blockquote::before {background: transparent !important;}';
+    echo "\n";
+    echo '</style>';
+    echo "\n";
+}
 ?>
 <html>
 <head>
@@ -20,28 +20,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="shortcut icon" href="<?php echo $GLOBALS['shortcut_ico'] ?>"type="image/x-icon" />
     <title><?php $this->archiveTitle(array(
-            'category'  =>  _t('%s'),
-            'search'    =>  _t('%s'),
-            'tag'       =>  _t('%s'),
-            'author'    =>  _t('%s')
+        'category'  =>  _t('%s'),
+        'search'    =>  _t('%s'),
+        'tag'       =>  _t('%s'),
+        'author'    =>  _t('%s')
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
 
-    <!-- 使用url函数转换相关路径 -->
-    <link type="text/css" rel="stylesheet" href="https://lib.baomitu.com/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/prism.css'); ?>">
-    <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/zoom.css'); ?>">
-    <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/main.css'); ?>">
-<?php if($GLOBALS['isIconNav'] == 'on'): ?>
-    <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/twemoji-awesome.css'); ?>">
-<?php endif; ?>
+        <!-- 使用url函数转换相关路径 -->
+        <link type="text/css" rel="stylesheet" href="https://lib.baomitu.com/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/prism.css'); ?>">
+        <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/zoom.css'); ?>">
+        <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/main.css'); ?>">
+        <?php if($GLOBALS['isIconNav'] == 'on'): ?>
+            <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/twemoji-awesome.css'); ?>">
+        <?php endif; ?>
 
     <!--[if lt IE 9]>
     <script src="http://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
     <script src="http://cdn.staticfile.org/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+<![endif]-->
 
-    <!-- 通过自有函数输出HTML头部信息 -->
-    <?php $this->header(); ?>
+<!-- 通过自有函数输出HTML头部信息 -->
+<?php $this->header(); ?>
 </head>
 <body>
 <!--[if lt IE 8]>
@@ -49,61 +49,62 @@
 <![endif]-->
 
 <header id="header" class="clearfix">
-<div class="container-fluid">
-    <div class="row">
-        <div class="logo">
-            <div class="header-logo">
-                <!-- 标题开始 -->
-                <span class="b">Y</span>
-                <span class="b">U</span>
-                <a href="<?php $this->options->siteUrl(); ?>">
-                    <span class="w">M</span>
-                </a>
-                <span class="b">O</span>
-                <span class="b">E</span>
-                <!-- 标题结束 -->
-                <a id="btn-menu" href="javascript:isMenu();">
-                    <span class="b">·</span>
-                </a>
-                <a href="javascript:isMenu1();">
-<?php if($GLOBALS['isIconNav'] == 'on'): ?>
-                    <span id="menu-1" class="bf"><i class="twa twa-flags"></i></span>
-<?php else: ?>
-                    <span id="menu-1" class="bf">1</span>
-<?php endif; ?>
-                </a>
-                <a href="javascript:isMenu2();">
-<?php if($GLOBALS['isIconNav'] == 'on'): ?>
-                    <span id="menu-2" class="bf"><i class="twa twa-evergreen-tree"></i></span>
-<?php else: ?>
-                    <span id="menu-2" class="bf">2</span>
-<?php endif; ?>
-                </a>
-                <a href="javascript:isMenu3();">
-<?php if($GLOBALS['isIconNav'] == 'on'): ?>
-                    <span id="menu-3" class="bf"><i class="twa twa-mag"></i></span>
-<?php else: ?>
-                    <span id="menu-3" class="bf">3</span>
-<?php endif; ?>
-                </a>
-            </div>
-            <div id="menu-page">
-            <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-            <?php while($pages->next()): ?>
-                <a href="<?php $pages->permalink(); ?>"><li><?php $pages->title(); ?></li></a>
-            <?php endwhile; ?>
-            <?php if($GLOBALS['isRSS'] == 'on'): ?>
-                <a href="<?php $this->options->feedUrl(); ?>"><li>RSS</li></a>
-            <?php endif; ?>
-            </div>
-            <div id="search-box">
-                <form id="search" method="post" action="./" role="search">
-                    <input autocomplete="off" type="text" name="s" id="menu-search" placeholder="Type something~" />
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-</header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="logo">
+                <div class="header-logo">
+                    <!-- 标题开始 -->
+                    <a href="<?php $this->options->adminUrl('login.php'); ?>">
+                     <span class="b"><?php echo $GLOBALS['NAME'][0]; ?></span></a>
+                     <span class="b"><?php echo $GLOBALS['NAME'][1]; ?></span>
+                     <a href="<?php $this->options->siteUrl(); ?>">
+                        <span class="w"><?php echo $GLOBALS['NAME'][2]; ?></span>
+                    </a>
+                    <span class="b"><?php echo $GLOBALS['NAME'][3]; ?></span>
+                    <span class="b"><?php echo $GLOBALS['NAME'][4]; ?></span>
+                    <!-- 标题结束 -->
+                    <a id="btn-menu" href="javascript:isMenu();">
+                        <span class="b">·</span>
+                    </a>
+                    <a href="javascript:isMenu1();">
+                        <?php if($GLOBALS['isIconNav'] == 'on'): ?>
+                            <span id="menu-1" class="bf"><i class="twa twa-flags"></i></span>
+                            <?php else: ?>
+                                <span id="menu-1" class="bf">1</span>
+                            <?php endif; ?>
+                        </a>
+                        <a href="javascript:isMenu2();">
+                            <?php if($GLOBALS['isIconNav'] == 'on'): ?>
+                                <span id="menu-2" class="bf"><i class="twa twa-evergreen-tree"></i></span>
+                                <?php else: ?>
+                                    <span id="menu-2" class="bf">2</span>
+                                <?php endif; ?>
+                            </a>
+                            <a href="javascript:isMenu3();">
+                                <?php if($GLOBALS['isIconNav'] == 'on'): ?>
+                                    <span id="menu-3" class="bf"><i class="twa twa-mag"></i></span>
+                                    <?php else: ?>
+                                        <span id="menu-3" class="bf">3</span>
+                                    <?php endif; ?>
+                                </a>
+                            </div>
+                            <div id="menu-page">
+                                <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                                <?php while($pages->next()): ?>
+                                    <a href="<?php $pages->permalink(); ?>"><li><?php $pages->title(); ?></li></a>
+                                <?php endwhile; ?>
+                                <?php if($GLOBALS['isRSS'] == 'on'): ?>
+                                    <a href="<?php $this->options->feedUrl(); ?>"><li>RSS</li></a>
+                                <?php endif; ?>
+                            </div>
+                            <div id="search-box">
+                                <form id="search" method="post" action="./" role="search">
+                                    <input autocomplete="off" type="text" name="s" id="menu-search" placeholder="Type something~" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
-<div id="body" class="clearfix">
+            <div id="body" class="clearfix">
