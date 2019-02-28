15fa006a60629432ecb3b651f038cf4a6b0b110a
yiny0liuyin <yi00oi9hhg@qq.com>
2019-02-28 19:45:52 +0800

    This bug has driven lots of coders completely mad. You won't believe how it ended up being fixed

--- footer.php
+++ footer.php
@@ -7,9 +7,9 @@
             <div class="col-12">
                 &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
                 <?php _e('Using <a target="_blank" href="http://www.typecho.org">Typecho</a> & <a target="_blank" href="https://yumoe.com/">Story+</a>'); ?>.
-                <?php if($GLOBALS['RUNTIME'] == JS) ?>
+                <?php if($GLOBALS['RUNTIME'] == JS): ?>
                 <p>本站已萌萌哒运行了 <span id="span_dt_dt"></span></p>
-                <?php else if($GLOBALS['RUNTIME'] == PHP;) ?>
+                <?php else if($GLOBALS['RUNTIME'] == PHP;): ?>
                 <?php
                 $sysReShow = (false !== ($sysInfo = sysInfo()))?"show":"none";
                 function sysInfo()
@@ -31,7 +31,7 @@
                     echo "本站已萌萌哒运行了".$sysInfo['uptime']."n";
                     echo "<br/>n";
                 }
-                else return;
+                
                 ?>
 
             <?php endif ?>
