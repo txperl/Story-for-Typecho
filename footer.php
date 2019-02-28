440ea49f8f30b2501f8cd969116b9d08a1bc80db
yiny0liuyin <yi00oi9hhg@qq.com>
2019-02-28 19:56:28 +0800

    We'll figure it out on Monday

--- footer.php
+++ footer.php
@@ -10,29 +10,7 @@
                 <?php if($GLOBALS['RUNTIME'] == 'JS'):?>
                 <p>本站已萌萌哒运行了 <span id="span_dt_dt"></span></p>
                 <?php elseif($GLOBALS['RUNTIME'] == 'PHP')
-              
-                $sysReShow = (false !== ($sysInfo = sysInfo()))?"show":"none";
-                function sysInfo()
-                {
-// UPTIME
-                    if (false === ($str = @file("/proc/uptime"))) return false;
-                    $str = explode(" ", implode("", $str));
-                    $str = trim($str[0]);
-                    $min = $str / 60;
-                    $hours = $min / 60;
-                    $days = floor($hours / 24);
-                    $hours = floor($hours - ($days * 24));
-                    $min = floor($min - ($days * 60 * 24) - ($hours * 60));
-                    if ($days !== 0) $res['uptime'] = $days."天";
-                    if ($hours !== 0) $res['uptime'] .= $hours."小时";
-                    $res['uptime'] .= $min."分钟";
-                    return $res;
-                }if("show"==$sysReShow){
-                    echo "本站已萌萌哒运行了".$sysInfo['uptime']."n";
-                    echo "<br/>n";
-                }
-                
-               
+                $this->need('time.php')
 
              endif ?>
         </div>
---
+++ time.php
@@ -0,0 +1,22 @@
+<?php
+$sysReShow = (false !== ($sysInfo = sysInfo()))?"show":"none";
+function sysInfo()
+{
+// UPTIME
+    if (false === ($str = @file("/proc/uptime"))) return false;
+    $str = explode(" ", implode("", $str));
+    $str = trim($str[0]);
+    $min = $str / 60;
+    $hours = $min / 60;
+    $days = floor($hours / 24);
+    $hours = floor($hours - ($days * 24));
+    $min = floor($min - ($days * 60 * 24) - ($hours * 60));
+    if ($days !== 0) $res['uptime'] = $days."天";
+    if ($hours !== 0) $res['uptime'] .= $hours."小时";
+    $res['uptime'] .= $min."分钟";
+    return $res;
+}if("show"==$sysReShow){
+    echo "本站已萌萌哒运行了".$sysInfo['uptime']."n";
+    echo "<br/>n";
+}
+?>       
