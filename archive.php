17d2e0014d9c5e810159ddc9b32132a7f4429c07
txperl <txperl@gmail.com>
2020-08-04 00:44:30 +0800

    bug fix
    - 修复 archive 封面图样式错误

--- archive.php
+++ archive.php
@@ -21,12 +21,12 @@
                 <?php if ($this->have()) : ?>
                     <?php while ($this->next()) : ?>
                         <li class="post-item grid-item" itemscope itemtype="http://schema.org/BlogPosting">
-                            <?php
-                            if (@$this->fields->cover) {
-                                echo '<span class="cover" style="background: url(' . $this->fields->cover . ') center/cover no-repeat;"></span>';
-                            }
-                            ?>
                             <a class="post-link" href="<?php $this->permalink() ?>">
+                                <?php
+                                if (@$this->fields->cover) {
+                                    echo '<span class="cover" style="background: url(' . $this->fields->cover . ') center/cover no-repeat;"></span>';
+                                }
+                                ?>
                                 <h3 class="post-title"><time class="index-time" datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('M j, Y'); ?></time><br><?php $this->title() ?></h3>
                                 <?php if ($this->category) : ?>
                                     <div class="post-meta">
